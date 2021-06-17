ccd2e69549b5fdb565bd20ec2f327fd94c6a1d13
Luke F. Lawson <luke@netlify.com>
2021-06-16 23:46:26 -0700

    add no-index header and update "build" command

--- base/copy-build.sh
+++ base/copy-build.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-FILE_LIST=(
-_redirects
-Page-2/
-green-river.png
-index.html
-large.jpg
-ads.txt
-.well-known/
-)
-
 FROM="pre/"
 TO="post/"
+FILE_LIST=($(ls -AF ${FROM}))
 
 echo "Logging of CI environment variable: CI=${CI}"
 echo "Logging of ARBITRARY environment variable: ARBITRARY=${ARBITRARY}"
