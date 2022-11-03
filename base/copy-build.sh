46c7afd9b818e31876435be1dd36e91d9cb6b484
Luke F. Lawson <luke@netlify.com>
2022-11-02 17:51:08 -0700

    remove static file list for autodection

--- base/copy-build.sh
+++ base/copy-build.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-FILE_LIST=(
-_redirects
-Page-2/
-green-river.png
-index.html
-large.jpg
-ads.txt
-.well-known/
-)
 
-FROM="pre/"
-TO="post/"
+FROM="./pre/"
+TO="./post/"
 
 echo "Logging of CI environment variable: CI=${CI}"
 echo "Logging of ARBITRARY environment variable: ARBITRARY=${ARBITRARY}"
@@ -19,9 +10,9 @@ echo "Logging of ARBITRARY environment variable: ARBITRARY=${ARBITRARY}"
 echo rm -Rf "${TO}"*
 rm -Rf "${TO}"*
 
-for i in "${FILE_LIST[@]}" ; do
-  if [ "${i:(-1)}" == "/" ] ; then
-    echo cp -R "${FROM}""${i%/}" "${TO}"
+for i in $(ls $FROM) ; do
+  if [[ -d "${FROM}${i}" ]] ; then
+    echo cp -R "${FROM}${i}" "${TO}"
     cp -R "${FROM}""${i%/}" "${TO}"
   else
     echo cp "${FROM}""$i" "${TO}"
@@ -34,3 +25,4 @@ cp ../netlify.toml .
 echo "Fake build complete!"
 
 exit 0
+
